aa4e1f6812c8a9d8b1f214ac52332bada57f3ab9
Julian Antonio Smith <juliansmith94@hotmail.com>
2020-03-08 14:10:23 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,19 +5,27 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  x = num1
+  y = num2 
+  x - y 
 end
 
 def division(num1, num2)
-
+  x = num1
+  y = num2 
+  num1/num2
 end
 
 def multiplication(num1, num2)
-
+  x = num1
+  y = num2
+  num1*num2
 end
 
 def modulo(num1, num2)
-
+  x=num1
+  y=num2
+  x%y
 end
 
 def square_root(num)
